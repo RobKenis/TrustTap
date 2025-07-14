45d7256ac35a2b5c4245c81655b7cc2cc87b5f9f
Rob Kenis <rob.kenis@hotmail.com>
2025-07-14 20:52:05 +0200

    Do not store duplicate IP addresses

--- internal/storage/in_memory.go
+++ internal/storage/in_memory.go
@@ -1,6 +1,11 @@
 package storage
 
-import "github.com/robkenis/TrustTap/internal/model"
+import (
+	"slices"
+
+	"github.com/robkenis/TrustTap/internal/model"
+	"github.com/rs/zerolog/log"
+)
 
 type InMemoryStorage struct {
 	requests []model.AccessRequest
@@ -13,10 +18,22 @@ func NewInMemoryStorage() *InMemoryStorage {
 }
 
 func (s *InMemoryStorage) Store(req model.AccessRequest) error {
-	s.requests = append(s.requests, req)
+	if s.shouldStore(req) {
+		s.requests = append(s.requests, req)
+		log.Info().Str("ip", req.IpAddress).Msg("Access request stored")
+	} else {
+		log.Info().Str("ip", req.IpAddress).Msg("Duplicate access request ignored")
+	}
 	return nil
 }
 
 func (s *InMemoryStorage) All() ([]model.AccessRequest, error) {
 	return s.requests, nil
+}
+
+func (s *InMemoryStorage) shouldStore(req model.AccessRequest) bool {
+	addressWasRequestedBefore := slices.ContainsFunc(s.requests, func(r model.AccessRequest) bool {
+		return r.IpAddress == req.IpAddress
+	})
+	return !addressWasRequestedBefore
 }
